096d444af48c784e776c53c936b35189c38df348
ranajaydas <ranajay@outlook.com>
2019-10-08 16:01:17 +0200

    Added Blueprints and Custom Error Pages.

---
+++ flaskblog/config.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '0bbe7a69ad9ecf8eced7a362d7948e4c'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
-from flaskblog import app           # importing from the user package(__init__.py)
+from flaskblog import create_app
 
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
